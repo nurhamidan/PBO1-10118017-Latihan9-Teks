e732f302d2daf3108b3e6f53007a0835672ea981
Agung Nurhamidan <>
2019-09-22 18:06:31 +0700

    First commit.

---
+++ src/pbo1/nim10118017/Main.java
@@ -0,0 +1,31 @@
+package pbo1.nim10118017;
+
+/**
+ *
+ * @author Agung Nurhamidan
+ * NAMA                 : Agung Nurhamidan
+ * KELAS                : PBO1
+ * NIM                  : 10118017
+ * Deskripsi Program    : Mendeklarasi dan mendeskripsikan variabel character.
+ *                        Mengakses dan menggabungkan variabelnya di dalam println.
+ *                        Menampilkannya di layar.
+ * 
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char c;
+        char tab;
+        char newLine;
+        
+        c = 'A';
+        tab = '\t';
+        newLine = '\n';
+        
+        System.out.println("Hasil:" + newLine + "Char" + tab + ": " + c);
+    }
+    
+}
